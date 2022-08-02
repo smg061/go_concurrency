1dce81ce396b6710020bee705348307f79887e80
Santos Gonzalez <max.go.95@gmail.com>
2022-08-02 00:42:08 -0500

    barbers shop problem

--- sleeping_barber/main.go
+++ sleeping_barber/main.go
@@ -22,22 +22,87 @@
 // semaphores (mutexes) is not needed.
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
 // variables
+
+var seatingCapacity = 10
+
+var arrivalRate = 100
+
+var cutDuration = 1000 * time.Millisecond
+
+var timeOpen = 10 * time.Second
 
 func main() {
 	// seed our random number generator
-
+	rand.Seed(time.Now().UnixNano())
 	// print welcome message
-
+	color.Yellow("The Sleeping Barber problem")
+	color.Yellow("--------------------------------")	
+	
 	// create channels if we need any
-
+	clientChan := make(chan string, seatingCapacity)
+	doneChan := make(chan bool)
 	// create the barbershop
+
+	shop := BarberShop {
+		ShopCapacity: seatingCapacity,
+		HairCutDuration: cutDuration,
+		NumberOfBarbers: 0,
+		ClientsChan: clientChan,
+		BarbersDoneChan: doneChan,
+		Open: true,
+	}
+
+	color.Green("The shop is open for the day!")
 
 	// add barbers
 
+	shop.addBarber("Frank")
+	shop.addBarber("Gerard")
+	shop.addBarber("Milton")
+	shop.addBarber("Susan")
+	shop.addBarber("Kelly")
+	shop.addBarber("Pat")
+
+
 	// start the barbershop as a goroutine
 
+	shopClosing := make(chan bool)
+	closed := make(chan bool)
+	go func() {
+
+		<- time.After(timeOpen)
+		shopClosing <- true
+
+		shop.closeShopForDay()
+		closed <- true
+	}()
 	// add clients
 
+	i:= 1
+
+	go func() {
+		for {
+			// get random number with average arrival arrival rate
+			randomMilliseconds := rand.Int() % (2* arrivalRate)
+			select {
+			case <- shopClosing:
+				return
+			case <- time.After(time.Millisecond * time.Duration(randomMilliseconds)):
+				shop.addClient(fmt.Sprintf("Client #%d", i))
+				i++
+			}
+		}
+	}()
+
 	// block until the barbershop is closed
+	<- closed
 }
